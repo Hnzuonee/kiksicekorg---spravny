925f5c1668e27843acb6fc847cbbf4bc30271622
Hnzuonee <harvillajan@gmail.com>
2025-05-20 00:22:22 +0200

    Update access.js

--- netlify/functions/access.js
+++ netlify/functions/access.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
   if (event.httpMethod !== "POST") {
     return {
@@ -8,7 +6,6 @@ exports.handler = async (event) => {
     };
   }
 
-  // Načtení tokenu z formuláře
   const formData = new URLSearchParams(event.body);
   const token = formData.get("cf-turnstile-response");
 
@@ -21,7 +18,6 @@ exports.handler = async (event) => {
 
   const secret = process.env.CF_TURNSTILE_SECRET;
 
-  // Ověření tokenu u Cloudflare
   const response = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
